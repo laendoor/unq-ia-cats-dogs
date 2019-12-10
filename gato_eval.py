76998f7afc24782edfca0959ede4a4f8bee7c1e3
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2019-12-09 22:41:57 -0300

    cso

--- gato_eval.py
+++ gato_eval.py
@@ -1,10 +1,11 @@
-import sklearn.metrics as skm
+import argparse
+import torch.nn as nn
 import matplotlib.pyplot as plt
-from catnet import CatNet
 import torch.utils.data.dataset
-from train_test import train, test, print_data, dataloader
+import sklearn.metrics as skm
+from catnet import CatNet
 from dataset import CatDogDataset
-
+from train_test import train, test, print_data, dataloader
 
 # def predict_image(image):
 #     image_tensor = test_transforms(image).float()
@@ -21,27 +22,22 @@ dataset_path = 'test_10'
 img_size = 32
 padding_mode = 'edge'
 batch_size = 84
-FILENAME_MODEL = 'gato_{:d}_model.pt'.format(epochs)
+FILENAME_MODEL = 'gato_{:d}.pt'.format(epochs)
 catdog_dataset = CatDogDataset(data_dir=dataset_path, img_size=img_size, padding_mode=padding_mode, label_mode='eval')
 
 validation_size = len(catdog_dataset)
 validation_dataset = torch.utils.data.random_split(catdog_dataset, [validation_size])
-validation_loader = dataloader(validation_dataset, batch_size)
+validation_loader = dataloader(validation_dataset[0], batch_size)
 
 # Loading model
-# model = CatNet()
-# model.load_state_dict(torch.load(FILENAME_MODEL))
-model = torch.load(FILENAME_MODEL)
+model = CatNet()
+model.load_state_dict(torch.load(FILENAME_MODEL))
 model.eval()  # modo evaluación
 
 # Predicciones para los datos de validación
 # Primero generamos la matriz de entradas y vector de resultados a partir del dataloader
 validation_inputs = list()
 validation_real = list()
-
-print(validation_loader.dataset)
-# exit(0)
-
 for batch, tensor in enumerate(validation_loader):
     value, output = tensor
     validation_inputs.append(value)
@@ -49,7 +45,17 @@ for batch, tensor in enumerate(validation_loader):
 
 inputs = torch.cat(validation_inputs)  # Se pasan a formato Tensor
 validation_real = torch.cat(validation_real)
-_, validation_predicted = torch.max(model(inputs), 1)  # Se obtienen las predicciones
+# _, validation_predicted = torch.max(model(inputs), 1)  # Se obtienen las predicciones
+validation_predicted = torch.sigmoid(torch.mean(model(inputs), 1))   # Se obtienen las predicciones
 
 print("validation real: ", validation_real)
-print("validation predicted: ", validation_predicted)
+print("validation inputs: ", validation_predicted)
+#
+# import csv
+#
+# with open('submission.csv', mode='w') as file:
+#     writer = csv.writer(file, delimiter='')
+#     writer.writerow(['id', 'valor'])
+
+for p in validation_predicted.detach().numpy():
+    print("{:.2f}".format(p))
